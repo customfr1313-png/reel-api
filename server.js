69e07d466e509d5d51d32d53d82cdb0652988049
Kunal Mehta <customfr1313@gmail.com>
2026-05-09 13:09:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const cors = require("cors");
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-key.json");
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+};
 
 // Firebase Init
 admin.initializeApp({
